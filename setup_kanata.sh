ff07a7d0376db52d2df389781dc56dd067eaecc8
Michael Neumann <michael_neumann@gmx.net>
2025-04-23 16:33:28 +0200

    add kanata

---
+++ setup_kanata.sh
@@ -0,0 +1,12 @@
+
+mkdir $HOME/aur-packages/
+git clone https://aur.archlinux.org/kanata.git $HOME/aur-packages/
+cd $HOME/aur-packages/kanata/
+makepkg
+
+sudo groupadd uinput
+sudo usermod -aG input $USER
+sudo usermod -aG uinput $USER
+sudo touch /etc/udev/rules.d/99-input.rules
+echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' | sudo tee -a /etc/udev/rules.d/99-input.rules
+echo "run sudo pacman -U 'name of kanata version'"
