97807ea7c9ef1c390df05725452a348a28c25106
Michael Neumann <michael_neumann@gmx.net>
2025-04-27 22:15:25 +0200

    change colosheme

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -120,7 +120,6 @@ return {
           -- end
         end,
       }
-      vim.cmd 'colorscheme rose-pine'
     end,
   },
 
@@ -131,6 +130,39 @@ return {
       require('vague').setup {
         -- optional configuration here
         -- vim.cmd.colorscheme 'vague',
+      }
+    end,
+  },
+
+  {
+    'rebelot/kanagawa.nvim',
+    config = function()
+      require('kanagawa').setup {
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {
+            fujiWhite = '#717C7C',
+          },
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = 'wave', -- Load "wave" theme
+        background = { -- map the value of 'background' option to a theme
+          dark = 'wave', -- try "dragon" !
+          light = 'lotus',
+        },
+        vim.cmd.colorscheme 'kanagawa',
       }
     end,
   },
